d7b9fcfcde13a474bb298fb5176be05f2164af06
raulpenate <50472064+raulpenate@users.noreply.github.com>
2022-07-09 09:19:56 -0600

    :construction_worker: Adding setxkmap to write with the layout on login

--- archinstall.sh
+++ archinstall.sh
@@ -408,6 +408,7 @@ exit
 
 # Setting my keyboard again as a KEYBOARDLAYOUT
 localectl set-x11-keymap us pc105 KEYBOARDLAYOUT
+setxkbmap -layout KEYBOARDLAYOUT
 
 # Installing yay
 git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
